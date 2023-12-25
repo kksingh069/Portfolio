00d14dce272df62989d6466eb14348c7f0b44f98
Krishna Kant <kantkrishna542@gmail.com>
2023-12-26 05:03:29 +0530

    home

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Home from "./Pages/Home";
+import Home from "./Pages/home";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
