6acc853e8c6642180a5d4401a3b105a0f4de6a07
Krishna Kant <kantkrishna542@gmail.com>
2023-12-26 05:02:08 +0530

    index.js fix

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Projects from "./Pages/Projects";
+import Contact from "./Pages/Contact";
+
+export {
+    Home,
+    About,
+    Projects,
+    Contact
+}
